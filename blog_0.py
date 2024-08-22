97e8881e9a097690a2e0ef2447930fc432bec883
osamaaraja <osamaakbarraja@yahoo.com>
2024-08-22 22:22:50 +0200

    fake login included fot testing

--- blog_0.py
+++ blog_0.py
@@ -41,9 +41,15 @@ def register():
 
     return render_template('register.html', title='Register', form=form)
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'admin':
+            flash(f"Logged in as {form.username}", "success")
+            return redirect(url_for('home'))
+        else:
+            flash(f"Invalid Login or Password", "danger")
     return render_template('login.html', title='Log In', form=form)
 
 
