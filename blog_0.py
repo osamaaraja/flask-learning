a997e3176cee6da6cbf4d3ff0f2b0c0dada8dc4c
osamaaraja <osamaakbarraja@yahoo.com>
2024-08-23 20:11:45 +0200

    post class added

--- blog_0.py
+++ blog_0.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from flask import Flask, render_template, flash, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from forms import LoginForm, RegistrationForm
@@ -16,6 +18,15 @@ class User(db.Model):
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
 # dummy data
 posts = [
     {
