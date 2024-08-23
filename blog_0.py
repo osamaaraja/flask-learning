8d81741d094264b03b63ad93599bcae637f72748
osamaaraja <osamaakbarraja@yahoo.com>
2024-08-23 19:59:50 +0200

    including db sqlite

--- blog_0.py
+++ blog_0.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template, flash, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 from forms import LoginForm, RegistrationForm
 app = Flask(__name__)
 
 
 app.config['SECRET_KEY'] = '123abc'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app)
 # dummy data
 posts = [
     {
