5c754c85bab20d9f0658bc6e8c9de7776b0824c2
osamaaraja <osamaakbarraja@yahoo.com>
2024-08-26 19:30:33 +0200

    templates moved in main package - blog

--- blog_0/routes.py
+++ blog_0/routes.py
@@ -1,7 +1,7 @@
 from blog_0.models import User, Post
 from flask import render_template, flash, redirect, url_for
 from blog_0.forms import LoginForm, RegistrationForm
-from blog_0 import app
+from blog_0 import app, db, bcrypt
 # dummy data
 posts = [
     {
@@ -34,8 +34,12 @@ def about():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash(f"Account created for {form.username.data}!", "success")
-        return redirect(url_for('home'))
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash(f"Your account has been created! You are now able to log in", "success")
+        return redirect(url_for('login'))
 
     return render_template('register.html', title='Register', form=form)
 
