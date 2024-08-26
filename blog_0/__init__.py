7dad60b29a517fe154125a6f1095a73a03941173
osamaaraja <osamaakbarraja@yahoo.com>
2024-08-26 19:28:42 +0200

    under main blog folder

--- blog_0/__init__.py
+++ blog_0/__init__.py
@@ -1,11 +1,13 @@
 # file where the application will be intialized and all the components will be brought together
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '123abc'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+
+bcrypt = Bcrypt(app)
 
 from blog_0 import routes
