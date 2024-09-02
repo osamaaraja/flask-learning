64b3d2d3544483833e52847d521dbee007711ca7
osamaaraja <osamaakbarraja@yahoo.com>
2024-09-02 23:25:59 +0200

    new post class added

--- blog_0/forms.py
+++ blog_0/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from blog_0.models import User
 
@@ -31,6 +31,10 @@ class LoginForm(FlaskForm):
 
     submit = SubmitField('Login')
 
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    content = TextAreaField('Content', validators=[DataRequired()])
 
+    submit = SubmitField('Post')
 
 
