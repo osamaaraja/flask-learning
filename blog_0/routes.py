bc592bb88801d227874a0830763f6a61f910900d
osamaaraja <osamaakbarraja@yahoo.com>
2024-09-10 20:29:31 +0200

    getting the different pages, by default page 1 set

--- blog_0/routes.py
+++ blog_0/routes.py
@@ -25,7 +25,7 @@ posts = [
 @app.route("/home")
 def home():
     page = request.args.get('page', 1, type=int)
-    posts = Post.query.paginate(per_page=5)
+    posts = Post.query.paginate(page=page, per_page=5)
     return render_template('home.html', posts=posts)
 
 @app.route("/about")
