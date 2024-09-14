b965e8e5ed6865cf26fd76fd0593e4dc41eddde8
osamaaraja <osamaakbarraja@yahoo.com>
2024-09-14 19:41:51 +0200

    request reset form

--- blog_0/forms.py
+++ blog_0/forms.py
@@ -41,3 +41,8 @@ class ResetResetForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     submit = SubmitField('Request Password Reset')
 
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is None:
+            raise ValidationError('There is no account with that email. You must register first.')
+
