a249f64f7c86f63348e5d4aef2a878829613b978
osamaaraja <osamaakbarraja@yahoo.com>
2024-09-17 20:59:26 +0200

    updating to include google mail

--- blog_0/__init__.py
+++ blog_0/__init__.py
@@ -3,6 +3,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '123abc'
@@ -12,5 +13,7 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
 
 from blog_0 import routes
