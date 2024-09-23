9b8e66aaf09a2f324eae3aee969f0dbd9e63586f
osamaaraja <osamaakbarraja@yahoo.com>
2024-09-23 22:11:05 +0200

    user specific moved

--- blog_0/routes.py
+++ blog_0/routes.py
@@ -37,41 +37,6 @@ def home():
 def about():
 
     return render_template('about.html', title='About')
-@app.route("/register", methods=['GET', 'POST'])
-def register():
-    if current_user.is_authenticated:
-        return redirect(url_for('home'))
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
-        db.session.add(user)
-        db.session.commit()
-        flash(f"Your account has been created! You are now able to log in", "success")
-        return redirect(url_for('login'))
-
-    return render_template('register.html', title='Register', form=form)
-
-@app.route("/login", methods=['GET', 'POST'])
-def login():
-    if current_user.is_authenticated:
-        return redirect(url_for('home'))
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
-        if user and bcrypt.check_password_hash(user.password, form.password.data):
-            login_user(user, remember=form.remember_me.data)
-            next_page = request.args.get('next')
-            return  redirect(next_page) if next_page else  redirect(url_for('home'))
-        else:
-            flash(f"Invalid Login or Password", "danger")
-    return render_template('login.html', title='Log In', form=form)
-
-
-@app.route("/logout")
-def logout():
-    logout_user()
-    return redirect(url_for('home'))
 
 def save_picture(form_picture):
     random_hex = secrets.token_hex(8)
