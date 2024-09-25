ade100e92f70d8de70b82f37c63e17e25c40eb43
osamaaraja <osamaakbarraja@yahoo.com>
2024-09-25 22:24:10 +0200

    filtering funcs

--- blog_0/users/routes.py
+++ blog_0/users/routes.py
@@ -2,6 +2,26 @@ from flask import Blueprint, render_template, request, redirect
 
 users = Blueprint('users', __name__)
 
+
+@app.route("/account", methods=['GET', 'POST'])
+@login_required
+def account():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your account has been updated!', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
+    return render_template('account.html', title='Account',
+                           image_file=image_file, form=form)
 @app.route("/register", methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
