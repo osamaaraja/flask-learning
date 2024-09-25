e8b98ea6f184801bedc89c0848c81928058ce23e
osamaaraja <osamaakbarraja@yahoo.com>
2024-09-25 22:25:11 +0200

    filtering funcs

--- blog_0/routes.py
+++ blog_0/routes.py
@@ -43,12 +43,7 @@ def about():
 
 
 
-@app.route("/user/<str:username>")
-def user_posts(username):
-    page = request.args.get('page', 1, type=int)
-    user = User.query.filter_by(username=username).first_or_404()
-    posts = Post.query.filter_by(author=user).order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
-    return render_template('user_posts.html', posts=posts, user=user)
+
 
 def send_reset_email(user):
     token = user.get_reset_token()
